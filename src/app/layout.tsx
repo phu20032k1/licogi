064d719fcd1859a68a937c6e5667ef4f51757514
Nguyen Minh Phu <phu20032k1@gmail.com>
2026-08-10 00:27:14 +0700

    Load homepage overview refinements

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import { siteConfig } from "../lib/siteConfig";
 import "./globals.css";
 import "./production.css";
 import "./map-professional.css";
+import "./home-overview.css";
 
 export const metadata: Metadata = {
   metadataBase: new URL(siteConfig.url),
