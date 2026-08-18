ce335e52356fcb89558264c18c7e1010a5f7b4d9
Nguyen Minh Phu <phu20032k1@gmail.com>
2026-08-19 06:45:54 +0700

    load mobile floating UI polish

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,7 @@ import "./public-typography-upgrade.css";
 import "./home-hero-asset.css";
 import "./mobile-polish.css";
 import "./mobile-icons.css";
+import "./mobile-floating.css";
 
 const beVietnamPro = Be_Vietnam_Pro({
   subsets: ["latin", "vietnamese"],
