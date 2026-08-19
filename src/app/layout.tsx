b8e88232c09fc89104b7af0d43d9702f87c2980a
Nguyen Minh Phu <phu20032k1@gmail.com>
2026-08-19 07:26:07 +0700

    Load phone redesign after legacy mobile styles

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ import "./mobile-icons.css";
 import "./mobile-floating.css";
 import "./mobile-universal.css";
 import "./photo-background.css";
+import "./phone-only-v2.css";
 
 const beVietnamPro = Be_Vietnam_Pro({
   subsets: ["latin", "vietnamese"],
